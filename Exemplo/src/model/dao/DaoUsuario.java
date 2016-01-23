650663f71508fec3186721537036c19434949594
Myllena <Denize@192.168.25.102>
2016-01-22 22:42:56 -0300

    Exemplo de Login com servlets e validação completa

---
+++ Exemplo/src/model/dao/DaoUsuario.java
@@ -0,0 +1,68 @@
+package model.dao;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+ 
+import model.bean.Usuario;
+ 
+public class DaoUsuario {
+ 
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd", "root", "ifpb");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } 
+        return connection;
+    }
+ 
+ 
+    public Usuario getUsuario( String login, String senha ){
+        Connection c = this.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try{
+            ps = c.prepareStatement("select id, nome from usuario where login = ? and senha = ?");
+            ps.setString(1, login);
+            ps.setString(2, senha);
+ 
+            rs = ps.executeQuery();
+ 
+            if ( rs.next() ){
+                Usuario user = new Usuario();
+                user.setId( rs.getInt("id") );
+                user.setLogin(login);
+                user.setSenha(senha);
+                user.setNome( rs.getString("nome") );
+ 
+                return user;
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally{
+            if (rs != null ) {
+                try { rs.close(); } catch (SQLException e) { ; }
+                rs = null;
+            }
+            if (ps != null ) {
+                try { ps.close(); } catch (SQLException e) { ; }
+                ps = null;
+            }
+            if (c != null ) {
+                try { c.close(); } catch (SQLException e) { ; }
+                c = null;
+            }
+        }
+        return null;
+    }
+ 
+}
